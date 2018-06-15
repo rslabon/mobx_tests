5aaa1cf62da7946dc013015ff0a4c85a5b83c2ed
Rafal Slabon <rafal.slabon@ocado.com>
2018-06-15 13:34:22 +0200

    add react router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "mobx-react";
+import createBrowserHistory from 'history/createBrowserHistory';
+import { RouterStore, syncHistoryWithStore } from 'mobx-react-router';
+import { Router } from 'react-router';
+
 import { MyStoreClass, MyStorePlain } from "./store";
 import { MyComponent } from "./component";
 
+const browserHistory = createBrowserHistory();
+const routingStore = new RouterStore();
+const history = syncHistoryWithStore(browserHistory, routingStore);
+
+
 const stores = {
+    routing: routingStore,
     myStorePlain: new MyStorePlain(),
     myStoreClass: new MyStoreClass()
 };
 
 ReactDOM.render(
     <Provider {...stores}>
-        <MyComponent />
+        <Router history={history}>
+            <MyComponent />
+        </Router>
     </Provider>,
     document.getElementById("root")
 );
